eba0c7af859ea214a988b79b4112436af0b154b7
a-latio <alex.latio@strathmore.edu>
2024-06-25 09:38:59 +0300

    update

--- loginpage.php
+++ loginpage.php
@@ -8,25 +8,17 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <class="container" id="signIN">
-        <h1 class="form-title">Sign In</h1>
-        <form method="post" action="register.php">
-            <div class="input-group">
-                <i class="fas fa-envelope"></i>
-                <input type="email" name="email" id="email" placeholder="Email" required>
-                <label for="email">Email</label>
-                <div class="input-group">
-                    <i class="fas fa-lock"></i>
-                    <input type="password" name="password" id="password" placeholder="Password" required>
-                    <label for="password">Password</label>
-                </div>
-                <p>
-                    <a href="#">Forgot Password?</a>
-                </p>
-                <input type="submit" class="btn" value="Sign In" name="signIN">
+<div class="container">
+        <h1>Login</h1>
+        <form>
+            <label for="username">Username</label>
+            <input type="text" id="username" name="username" placeholder="email">
+            <label for="password">Password</label>
+            <input type="password" id="password" name="password" placeholder="password" required>
+            <button type="submit">Log In</button>
         </form>
     </div>
     <script src="script.js"></script>
 </body>
 </html>
-//wayne's update
+
