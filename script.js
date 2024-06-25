d2ebab4ed1d96a90c8c3fe1d9c111c10c28e78c9
a-latio <alex.latio@strathmore.edu>
2024-06-25 08:24:28 +0300

    login

---
+++ script.js
@@ -0,0 +1,6 @@
+const signInButton=document.getElementById('signInButton');
+const signInForm=document.getElementById('signIn');
+
+signInButton.addEventListener('click',function(){
+    signInForm.style.display='block';
+})
