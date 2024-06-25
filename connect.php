d4686c68a2b4c881f5a9f4fe33fda502a8d28791
a-latio <alex.latio@strathmore.edu>
2024-06-25 10:03:19 +0300

    update

--- connect.php
+++ connect.php
@@ -6,7 +6,8 @@ $db="login_db"
 
 $connect = new mysqli($host, $user, $password, $db);
 
-if ($connect->connect_error) {
+if ($conn->connect_error) {
     echo "Failed to connect DB" .$conn->connect_error;
 }
+echo "Connected Successfully";
 ?>
