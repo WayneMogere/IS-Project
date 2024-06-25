1a1ff51a4a9b47bc8b87d1cd4331c0023feb53bc
a-latio <alex.latio@strathmore.edu>
2024-06-25 11:33:45 +0300

    update

--- loginpage.php
+++ loginpage.php
@@ -4,7 +4,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
-    <link rel="stylesheet" href="style.css">
 </head>
 <body>
 <div class="container">
@@ -17,7 +16,6 @@
             <button type="submit">Log In</button>
         </form>
     </div>
-    <script src="script.js"></script>
 </body>
 </html>
 
--- script.js
+++
@@ -1,6 +0,0 @@
-const signInButton=document.getElementById('signInButton');
-const signInForm=document.getElementById('signIn');
-
-signInButton.addEventListener('click',function(){
-    signInForm.style.display='block';
-})
