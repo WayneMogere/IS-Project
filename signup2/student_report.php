2693cecfe223f8d3f4d9fdf9cd3a255498ed27a3
WayneMogere <mogere.wayne@strathmore.edu>
2024-07-19 18:27:43 +0300

    report page update

--- signup2/student_report.php
+++ signup2/student_report.php
@@ -7,16 +7,25 @@ $email = $_POST['email'];
 $studentid = $_POST['studentid'];
 $description = $_POST['description'];
 
-$sql="INSERT INTO `report`(`name`, `email`, `studentid`, `description`)
-    VALUES('$name', '$email','$studentid', '$description')";
+$sql="Select * from `report` where description = '$description'";
 
 $result=mysqli_query($conn,$sql);
 if($result){
-    echo'Thank you for your help';
-    header('location:student_dashboard.html');
-  }
-else{
-    die(mysqli_error($conn));
-}
+    $num=mysqli_num_rows($result);
+    if($num>0){
+        echo "Item already reported. <a style='color: black;' href='student_report.html'>Go Back</a>";
+        
 
+    }else{
+        $sql="INSERT INTO `report`(`name`, `email`, `studentid`, `description`)
+        VALUES('$name', '$email','$studentid', '$description')";
+        $result=mysqli_query($conn,$sql);
+        if($result){
+            echo "Thank you for your help. <a style='color: black;' href='student_report.html'>Go Back</a>";
+        }
+        else{
+            die(mysqli_error($conn));
+        }
+    }
+}
 ?>
